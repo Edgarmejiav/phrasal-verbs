fc842ddf95bb4c258554e02fe1f5c95b986ea4d3
edgarmejiav <edgarm_10@hotmail.com>
2023-05-15 19:37:01 -0500

    phrasal-verbs

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,137 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, {useState, useEffect} from 'react';
+import Confetti from 'react-confetti'
+const verbs = [
+    {
+        "verb": "Look after",
+        "description": "To take care of or watch over someone or something"
+    },
+    {
+        "verb": "Give up",
+        "description": "To abandon something or give up on it"
+    },
+    {
+        "verb": "Get along",
+        "description": "To have a good relationship with someone"
+    },
+    {
+        "verb": "Put off",
+        "description": "To postpone something for another time"
+    }]
 
-const inter = Inter({ subsets: ['latin'] })
+function getRandomPosition(array) {
+    if (array.length === 0) {
+        return undefined; // Manejar el caso de un array vacío según tus necesidades
+    }
+
+    const randomIndex = Math.floor(Math.random() * array.length);
+
+    return array[randomIndex];
+}
+
+function generateHints(word) {
+    return Array.from(word, (char) => (char === ' ' ? '*' : '_'));
+}
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [rWord, setRWord] = useState(getRandomPosition(verbs))
+    const [word, setWord] = useState(rWord.verb);
+    const [description, setDescription] = useState(rWord.description);
+
+    const [guess, setGuess] = useState('');
+    const [gameWon, setGameWon] = useState(false);
+    const [hints, setHints] = useState([]);
+
+    useEffect(() => {
+        if (word && word !== '') {
+            setHints(generateHints(word));
+        }
+
+    }, [word]);
+
+
+    const handleGuess = () => {
+        const formattedGuess = guess.toLowerCase();
+        console.log(formattedGuess, word)
+        if (formattedGuess === word.toLowerCase()) {
+            setGameWon(true);
+        } else {
+            setGuess('');
+        }
+    };
+
+
+    const revealHint = () => {
+        const hintArray = [...hints];
+        const unrevealedIndexes = [];
+
+        hintArray.forEach((hint, index) => {
+            if (hint === '_') {
+                unrevealedIndexes.push(index);
+            }
+        });
+
+        if (unrevealedIndexes.length === 0) {
+            setGameWon(true);
+            return;
+        }
+
+        const randomIndex = unrevealedIndexes[Math.floor(Math.random() * unrevealedIndexes.length)];
+        hintArray[randomIndex] = word[randomIndex];
+        setHints(hintArray);
+    };
+
+    function nextStep() {
+        setGameWon(false);
+        setHints([]);
+        setGuess('');
+
+        const rWord = getRandomPosition(verbs);
+
+        setDescription(rWord.description ?? "");
+        setWord(rWord.verb ?? "");
+        setHints(generateHints(rWord.verb));
+
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+
+            {/*{gameWon &&<Confetti*/}
+            {/*    width={400}*/}
+            {/*    height={400}*/}
+            {/*    tweenDuration={100}*/}
+            {/*    className={"items-center justify-center"}*/}
+            {/*/> }*/}
+            <div className="bg-gray-700 p-6 rounded-lg shadow-lg">
+                <h1 className="mb-4 text-3xl text-center font-bold">Verbs Phrasals</h1>
+                {gameWon && (
+                    <strong className="mb-4 text-2xl font-bold">{word}</strong>
+                )}
+                <p className="mb-3">Hints: {hints.join(' ')}</p>
+
+                <h1 className="mb-3 text-white">{description}</h1>
+                <input
+                    className=" mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    type="text" value={guess} onChange={(e) => setGuess(e.target.value)}/>
+                <div className="flex justify-between my-2">
+                    <button className="mx-2 flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={handleGuess}>Intentar
+                    </button>
+                    <button className="mx-2 flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={revealHint}>Pista
+                    </button>
+                </div>
+                <div className="flex justify-between my-2">
+
+                    <button
+                        className={!gameWon ? ` mx-2 flex-1 bg-gray-500 text-white font-bold py-2 px-4 rounded  ` : ` mx-2 flex-1 bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded`}
+                        disabled={!gameWon} onClick={nextStep}>Siguiente
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
