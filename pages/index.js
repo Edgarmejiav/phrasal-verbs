53871a6feb9cbb42ac2191a78eacb96b2badd7fa
Edgarmejiav <edgarm_10@hotmail.com>
2023-05-16 09:24:14 -0500

    add json, cofetti

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import Confetti from 'react-confetti'
-const verbs = [
-    {
-        "verb": "Look after",
-        "description": "To take care of or watch over someone or something"
-    },
-    {
-        "verb": "Give up",
-        "description": "To abandon something or give up on it"
-    },
-    {
-        "verb": "Get along",
-        "description": "To have a good relationship with someone"
-    },
-    {
-        "verb": "Put off",
-        "description": "To postpone something for another time"
-    }]
+
+import verbs from './verbs.json';
+import {IconVolumen} from "@/pages/IconVolumen";
 
 function getRandomPosition(array) {
     if (array.length === 0) {
@@ -93,39 +79,51 @@ export default function Home() {
 
     }
 
+    function handleVoice() {
+        const text = new SpeechSynthesisUtterance(word)
+        speechSynthesis.speak(text)
+    }
+
     return (
         <div className="min-h-screen flex items-center justify-center">
-
-            {/*{gameWon &&<Confetti*/}
-            {/*    width={400}*/}
-            {/*    height={400}*/}
-            {/*    tweenDuration={100}*/}
-            {/*    className={"items-center justify-center"}*/}
-            {/*/> }*/}
-            <div className="bg-gray-700 p-6 rounded-lg shadow-lg">
-                <h1 className="mb-4 text-3xl text-center font-bold">Verbs Phrasals</h1>
-                {gameWon && (
-                    <strong className="mb-4 text-2xl font-bold">{word}</strong>
+            {gameWon &&<Confetti
+                width={window.innerWidth}
+                height={window.innerHeight}
+                tweenDuration={100}
+                className={"items-center justify-center"}
+            /> }
+            <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+                <h1 className="mb-4 text-4xl text-white text-center font-bold">Verbs Phrasals</h1>
+                {!gameWon && (<p className="mb-3 text-white">Hints: {hints.join(' ')}</p>
                 )}
-                <p className="mb-3">Hints: {hints.join(' ')}</p>
+                {gameWon && (
+                    <div className={"flex"}>
+                        <strong className="mb-4 mr-4 text-white text-center  text-3xl font-bold">{word}</strong>
+                        <IconVolumen onClick={handleVoice}/>
+                    </div>
 
-                <h1 className="mb-3 text-white">{description}</h1>
+                )}
+                <figcaption className="mb-3 text-white">{description}</figcaption>
+
                 <input
-                    className=" mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    disabled={gameWon}
+                    className="my-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text" value={guess} onChange={(e) => setGuess(e.target.value)}/>
                 <div className="flex justify-between my-2">
-                    <button className="mx-2 flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={handleGuess}>Intentar
+                    <button
+                        className={`  flex-1 font-bold text-white mr-1  py-2 px-4 rounded ${gameWon ? `  bg-gray-600 hover:bg-gray-500  ` : `bg-blue-500 hover:bg-blue-700   `} `}
+                        disabled={gameWon} onClick={handleGuess}>try
                     </button>
-                    <button className="mx-2 flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={revealHint}>Pista
+                    <button
+                        className={`  flex-1 font-bold text-white ml-1  py-2 px-4 rounded ${gameWon ? `  bg-gray-600 hover:bg-gray-500  ` : `bg-blue-500 hover:bg-blue-700   `} `}
+                        disabled={gameWon} onClick={revealHint}>hint
                     </button>
                 </div>
                 <div className="flex justify-between my-2">
 
                     <button
-                        className={!gameWon ? ` mx-2 flex-1 bg-gray-500 text-white font-bold py-2 px-4 rounded  ` : ` mx-2 flex-1 bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded`}
-                        disabled={!gameWon} onClick={nextStep}>Siguiente
+                        className={`flex-1 text-white   py-2 px-4 rounded   font-bold   ${!gameWon ? `   bg-gray-500` : `   bg-green-700 hover:bg-green-500 `}`}
+                        disabled={!gameWon} onClick={nextStep}>next
                     </button>
                 </div>
             </div>
