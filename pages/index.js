a1a4ec3c2bc56c546fa5dac10bfce6483ee0baee
Edgarmejiav <edgarm_10@hotmail.com>
2023-05-19 12:25:10 -0500

    refactor: change verbs to .json

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Confetti from 'react-confetti'
 import IconVolumen from "@/pages/components/IconVolumen";
-import {verbs} from "@/verbs";
+import verbs from "@/verbs.json";
 
 
 function getRandomPosition(array) {
